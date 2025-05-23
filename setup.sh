335e98d1b31f7ea801d8168e028261abc3b1c191
Karan Kataria <karankataria@Karans-MacBook-Pro.local>
2025-05-24 00:03:36 +0100

    Added a setup bash shell script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
+# Create virtual enviroment to prevent clashing dependencies
 python3 -m venv venv
 
+# Create a virtual enviroment
 source venv/bin/activate
 
+# Install all required dependencies
 pip3 install -r requirements.txt
