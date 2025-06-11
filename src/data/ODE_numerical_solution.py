2010fe1629c310a291e15c53e30989f5e85fa33d
Karan Kataria <karankataria@Karans-MacBook-Pro.local>
2025-06-12 00:46:01 +0100

    Sucessfully ran the first test case

--- src/data/ODE_numerical_solution.py
+++ src/data/ODE_numerical_solution.py
@@ -1,1 +1,86 @@
-import scipy as sp
+"""
+This script numerically solves the system of first-order ODEs
+using Runge-Kutta 4,5. This enables the generation of synthetic
+data.
+"""
+
+# Import in the required packages and functions
+from scipy.integrate import solve_ivp
+import numpy as np
+import matplotlib.pyplot as plt
+import scienceplots    # Used to make plots more scientific
+
+# Import in the swing equation converted into a system of first-order ODEs
+from swing_equation import swing_equation
+
+# Define absolute path to save .npz files in
+PATH: str = '/Users/karankataria/Library/CloudStorage/OneDrive-UniversityofWarwick/dissertation_code/data/'
+
+def swing_ODEs_solver(
+    initial_time: float, initial_state: np.array, final_time: float, timestep: float,
+    inertia: float, damping: float, mechanical_power: float,
+    voltage_magnitude: float, include_controllers: bool, voltages: np.array, phase_angles: np.array,
+    susceptances: np.array, file_name: str, save_output_to_file: bool = False, **kwargs) -> tuple[np.array, np.array]:
+   
+    t0 = initial_time
+    y0 = initial_state
+    tN = final_time
+
+    func = lambda time, state_vec: swing_equation(
+        inertia=inertia, damping=damping, state=state_vec, mechanical_power=mechanical_power,
+        voltage_magnitude=voltage_magnitude, include_controllers=include_controllers, voltages=voltages,
+        phase_angles=phase_angles, susceptances=susceptances, **kwargs
+        )
+
+    discretisation = np.arange(start=t0, stop=tN+timestep, step=timestep)
+
+    numerical_solution = solve_ivp(fun=func, t_span=(t0, tN), y0=y0, method='RK45', t_eval=discretisation)
+
+    solution = numerical_solution.y
+    times = numerical_solution.t
+
+    if save_output_to_file:
+        np.savez(file=PATH+file_name, phase_angle=solution[0,:], angular_freq=solution[1,:], times=times)
+
+    return (solution, times)
+
+if __name__ == '__main__':
+    """
+    This section defines an arbitrary test case to test the numerical solver
+    functionality.
+    """
+    plt.style.use('science')
+
+    timestep = 0.1
+    initial_time = 0
+    final_time = 20.0
+
+    initial_state = np.array([0.1, 0.1])
+
+    inertia = 0.2
+    damping = 0.1
+    mechanical_power = 0.1
+    voltage = 1.0
+    voltages = np.array([1.0])
+    susceptances = np.array([0.2])
+    phase_angles = np.array([0.0])
+
+    solution, times = swing_ODEs_solver(
+        initial_time=initial_time, initial_state=initial_state, final_time=final_time, timestep=timestep,
+        inertia=inertia, damping=damping, mechanical_power=mechanical_power, voltage_magnitude=voltage, include_controllers=False,
+        voltages=voltages, phase_angles=phase_angles, susceptances=susceptances, file_name='test_run', save_output_to_file=False
+    )
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].plot(times, solution[0,:], linestyle='-.', color='red')
+
+    ax[1].plot(times, solution[1,:], linestyle='-.', color='red')
+    ax[1].axhline(0, color='gray', linestyle='--')
+
+    ax[0].set_xlabel('Time (s)', fontsize=12)
+    ax[0].set_ylabel('Phase Angle $\delta$ (rad)', fontsize=12)
+
+    ax[1].set_xlabel('Time (s)', fontsize=12)
+    ax[1].set_ylabel('Angular Frequency $\dot{\delta}$ (rad/s)', fontsize=12)
+
+    plt.show()
--- src/data/swing_equation.py
+++ src/data/swing_equation.py
@@ -9,12 +9,12 @@ Supervisor: Dr. Subhash Lakshminarayana
 """
 
 # Import in the required functions 
-from numpy import sin
+import numpy as np 
 
 def swing_equation(
-        inertia: float, damping: float, phase_angle: float, angular_frequency: float,
-        mechanical_power: float, voltage_magnitude: float, include_controllers: bool = False,
-        *voltages, *phase_angles, *susceptances, **kwargs) -> np.array:
+        inertia: float, damping: float, state: np.array,
+        mechanical_power: float, voltage_magnitude: float, include_controllers: bool,
+        voltages: np.array, phase_angles: np.array, susceptances: np.array, **kwargs) -> np.array:
     """
     This function defines the swing equation for a given generator, including
     and exluding controllers, and returns a system of first order ODEs to be
@@ -23,25 +23,28 @@ def swing_equation(
         Inputs:
                 inertia: The inertia coefficient for the given generator
                 damping: The damping coefficient for the given generator
-                phase_angle: The phase angle for the given generator (rads)
-                angular_frequency: The angle frequency for the given generator (rads/s)
-                mechanical_power: The mechanical power being supplied to the generator (p.u)
-                voltage_magnitude: The voltage being supplied by the generator (p.u)
+                state: The phase angle (rad) and angular frequency (rad/s) for the given generator
+                mechanical_power: The mechanical power being supplied to the generator (p.u.)
+                voltage_magnitude: The voltage being supplied by the generator (p.u.)
                 include_controllers: Boolean arguement to determine whether or not controllers should be included
-                voltages: (*args arguements) The voltages of the other generators in the power system
-                phase_angles: (*args arguements) The phase angles of the other generators in the power system
-                susceptances: (*args arguement) The susceptance values between other generators and the given generator
+                voltages: The voltages of the other generators in the power system (p.u.)
+                phase_angles: The phase angles of the other generators in the power system (rad)
+                susceptances: The susceptance values between other generators and the given generator (p.u.)
                 **kwargs: The controller coefficients (integral and proportional)
 
         Outputs:
                 eta_1_dot: The time-derivative of the first dummy variable (equal to the angular frequency)
                 eta_2_dot: The time-derivative of the second dummy variable (equal to the angular acceleration)
     """
+
+    # Extract the phase angle and angular frequencies from the state vector
+    phase_angle = state[0]
+    angular_frequency = state[1] 
     
     # Compute the total electrical power output generator k supplies to the grid
     total_electrical_output = 0
     for v, delta, B in zip(voltages, phase_angles, susceptances):
-        total_electrical_output += B * voltage_magnitude * v * sin(phase_angle - delta)
+        total_electrical_output += B * voltage_magnitude * v * np.sin(phase_angle - delta)
 
     # Define dummy variables for converting second-order ODE to system of first order ODEs
     eta_1 = phase_angle
@@ -65,6 +68,3 @@ def swing_equation(
 
         # Return a system of first-order ODEs
         return np.array([eta_1_dot, eta_2_dot])
-
-def swing_ODEs(time: float, state: np.array) -> float:
-    ...
