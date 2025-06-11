5dd98bb66b80a4992e9acdb39f28049adf7e6fa3
Karan Kataria <karankataria@Karans-MacBook-Pro.local>
2025-06-11 22:41:00 +0100

    Defined the swing equations, including and excluding the controllers, as a system of first-order ODEs

--- src/data/swing_equation.py
+++ src/data/swing_equation.py
@@ -1,4 +1,70 @@
-import numpy as np
+"""
+This file defines the swing equations (inclusive and exclusive of controllers)
+to be used in the ODE_numerical_solution.py file for numerically solving the ODEs
+and obtaining synthetic data.
 
-def swing_equation(time: float, include_controllers: bool) -> float:
-    pass
+Author: Karan Kataria
+Work: MSc thesis
+Supervisor: Dr. Subhash Lakshminarayana
+"""
+
+# Import in the required functions 
+from numpy import sin
+
+def swing_equation(
+        inertia: float, damping: float, phase_angle: float, angular_frequency: float,
+        mechanical_power: float, voltage_magnitude: float, include_controllers: bool = False,
+        *voltages, *phase_angles, *susceptances, **kwargs) -> np.array:
+    """
+    This function defines the swing equation for a given generator, including
+    and exluding controllers, and returns a system of first order ODEs to be
+    passed into a numerical solver (e.g. RK45).
+
+        Inputs:
+                inertia: The inertia coefficient for the given generator
+                damping: The damping coefficient for the given generator
+                phase_angle: The phase angle for the given generator (rads)
+                angular_frequency: The angle frequency for the given generator (rads/s)
+                mechanical_power: The mechanical power being supplied to the generator (p.u)
+                voltage_magnitude: The voltage being supplied by the generator (p.u)
+                include_controllers: Boolean arguement to determine whether or not controllers should be included
+                voltages: (*args arguements) The voltages of the other generators in the power system
+                phase_angles: (*args arguements) The phase angles of the other generators in the power system
+                susceptances: (*args arguement) The susceptance values between other generators and the given generator
+                **kwargs: The controller coefficients (integral and proportional)
+
+        Outputs:
+                eta_1_dot: The time-derivative of the first dummy variable (equal to the angular frequency)
+                eta_2_dot: The time-derivative of the second dummy variable (equal to the angular acceleration)
+    """
+    
+    # Compute the total electrical power output generator k supplies to the grid
+    total_electrical_output = 0
+    for v, delta, B in zip(voltages, phase_angles, susceptances):
+        total_electrical_output += B * voltage_magnitude * v * sin(phase_angle - delta)
+
+    # Define dummy variables for converting second-order ODE to system of first order ODEs
+    eta_1 = phase_angle
+    eta_2 = angular_frequency
+    
+    eta_1_dot = eta_2
+    
+    # If controllers should be included, return the system of 1st order ODEs with controller coefficients
+    if include_controllers:
+        controller_proportional = kwargs['controller_proportional']
+        controller_integral = kwargs['controller_integral']
+        
+        eta_2_dot = (1 / inertia) * (controller_integral * phase_angle - total_electrical_output - (damping - controller_proportional) * angular_frequency)
+
+        # Return a system of first-order ODEs
+        return np.array([eta_1_dot, eta_2_dot])
+
+    # Else return the system of 1st order ODEs without any controller coefficients
+    else:
+        eta_2_dot = (1 / inertia) * (mechanical_power - total_electrical_output - damping * angular_frequency) 
+
+        # Return a system of first-order ODEs
+        return np.array([eta_1_dot, eta_2_dot])
+
+def swing_ODEs(time: float, state: np.array) -> float:
+    ...
